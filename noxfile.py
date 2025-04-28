20a3f46393ca25977ad10c592abd2b6da5639e82
shenxianpeng <xianpeng.shen@gmail.com>
2025-04-29 01:04:27 +0300

    Add new pip issue 13353 test

--- noxfile.py
+++ noxfile.py
@@ -7,3 +7,9 @@ def test_issue_13359(session: nox.Session) -> None:
     """Test for https://github.com/pypa/pip/issues/13359"""
     session.install("pip==25.1")
     session.install("PySide6")
+
+@nox.session
+def test_issue_13353(session: nox.Session) -> None:
+    """Test for https://github.com/pypa/pip/issues/13353"""
+    session.install("pip==25.1")
+    session.install("aiohttp[speedups] == 3.11.16")
