23af129fedd01da91e5063ac0f9d69eae58de202
shenxianpeng <xianpeng.shen@gmail.com>
2025-04-29 00:31:48 +0300

    Remove lint from noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -2,12 +2,6 @@ import nox
 
 nox.options.reuse_existing_virtualenvs = True
 
-@nox.session(python=["3.13"])
-def lint(session: nox.Session) -> None:
-    """Run linters."""
-    session.install("pre-commit")
-    session.run("pre-commit", "run", "--all-files")
-
 @nox.session
 def test_issue_13359(session: nox.Session) -> None:
     """Test for https://github.com/pypa/pip/issues/13359"""
